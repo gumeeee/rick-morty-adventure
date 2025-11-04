96cf811b367b9e7b7ebec81163ceae8a7b035f61
gumeeee <gaxco123@gmail.com>
2025-11-04 11:22:23 -0300

    feat(episodes): add integration with TMDB API for episode ratings

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://rickandmortyapi.com/api',
+  tmdbApiKey: 'YOUR_TMDB_API_KEY_HERE',
+  tmdbBaseUrl: 'https://api.themoviedb.org/3',
+  tmdbImageBaseUrl: 'https://image.tmdb.org/t/p/w500',
 };
